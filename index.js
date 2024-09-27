99309fdb3d6ffdf17ca70bec178ba6b1f18f4762
hassan <hassanibrahimndahiro@gmail.com>
2024-09-28 00:34:55 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+//variable for current user
+const currentUser = 'John Doe';
+
+
+//welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//welcome message to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+//short greeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
